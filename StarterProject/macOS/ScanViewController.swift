192ddfeb774035e03b90af70f8177a955ac9149e
Jack Noh <jaegyu.star@gmail.com>
2023-01-17 17:46:50 +0900

    imp: quaternion data streaming

--- StarterProject/macOS/ScanViewController.swift
+++ StarterProject/macOS/ScanViewController.swift
@@ -14,11 +14,14 @@ class ScanViewController: NSViewController, NSTableViewDelegate, NSTableViewData
     @IBOutlet weak var tableView: NSTableView!
     
     var scannerModel: ScannerModel!
+    var isStreaming: Bool!
 
     // MARK: View Life Cycle
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+
+        isStreaming = false
+
         tableView.target = self
         tableView.doubleAction = #selector(ScanViewController.tableViewDoubleClick(sender:))
         scannerModel = ScannerModel(delegate: self)
@@ -55,7 +58,10 @@ class ScanViewController: NSViewController, NSTableViewDelegate, NSTableViewData
         }
         
         let connected = cell.viewWithTag(3) as! NSTextField
-        if device.isConnectedAndSetup {
+        if isStreaming {
+            connected.stringValue = "Data streaming!"
+            connected.isHidden = false
+        } else if device.isConnectedAndSetup {
             connected.stringValue = "Connected!"
             connected.isHidden = false
         } else if scannerModel.items[row].isConnecting {
@@ -92,14 +98,45 @@ class ScanViewController: NSViewController, NSTableViewDelegate, NSTableViewData
     
     @objc func tableViewDoubleClick(sender: AnyObject) {
         let device = scannerModel.items[tableView.clickedRow].device
+
         guard !device.isConnectedAndSetup else {
-            device.flashLED(color: .red, intensity: 1.0, _repeat: 3)
-            mbl_mw_debug_disconnect(device.board)
-            DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
-                self.tableView.reloadData()
+            let board = device.board
+            let signal = mbl_mw_sensor_fusion_get_data_signal(board, MBL_MW_SENSOR_FUSION_DATA_QUATERNION)
+
+            guard !isStreaming else {
+              mbl_mw_sensor_fusion_stop(board)
+              mbl_mw_sensor_fusion_clear_enabled_mask(board)
+              mbl_mw_datasignal_unsubscribe(signal)
+
+              device.flashLED(color: .red, intensity: 1.0, _repeat: 3)
+              mbl_mw_debug_disconnect(device.board)
+              DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+                  self.isStreaming = false
+                  self.tableView.reloadData()
+              }
+              return
             }
+
+            mbl_mw_sensor_fusion_set_acc_range(board, MBL_MW_SENSOR_FUSION_ACC_RANGE_16G)
+            mbl_mw_sensor_fusion_set_gyro_range(board, MBL_MW_SENSOR_FUSION_GYRO_RANGE_2000DPS)
+            mbl_mw_sensor_fusion_set_mode(board, MBL_MW_SENSOR_FUSION_MODE_NDOF)
+            mbl_mw_sensor_fusion_write_config(board)
+
+            mbl_mw_datasignal_subscribe(signal, bridge(obj: self)) { (context, obj) in
+              let quaternion: MblMwQuaternion = obj!.pointee.valueAs()
+              print(Double(quaternion.x),Double(quaternion.y), Double(quaternion.z), Double(quaternion.w))
+            }
+
+            mbl_mw_sensor_fusion_clear_enabled_mask(board)
+            mbl_mw_sensor_fusion_enable_data(board, MBL_MW_SENSOR_FUSION_DATA_QUATERNION)
+            mbl_mw_sensor_fusion_write_config(board)
+            mbl_mw_sensor_fusion_start(board)
+
+            isStreaming = true
+            self.tableView.reloadData()
             return
         }
+
         scannerModel.items[tableView.clickedRow].toggleConnect()
         tableView.reloadData()
     }
